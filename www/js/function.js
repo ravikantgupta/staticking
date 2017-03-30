525383a2b75e65bd290f4e58f516a13059d3e963
Ravi <ravi@orangespike.com>
2017-03-30 13:14:13 +0530

    fghfghfgh

--- www/js/function.js
+++ www/js/function.js
@@ -1,4 +1,15 @@
 
+var app = {
+	// Application Constructor
+	initialize: function() {
+		this.bindEvents();
+	},
+
+	bindEvents: function() {
+		document.addEventListener('deviceready', app.onDeviceReady, false);
+	}
+	
+};
 
 
 	            document.addEventListener("deviceready", function() {
