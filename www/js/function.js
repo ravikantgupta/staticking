b1b44ac3030e20e1bb28e689b50c4f4d5894b05e
Ravikant <rvgupta444@gmail.com>
2017-04-01 10:49:27 +0530

    gjghjhgj

--- www/js/function.js
+++ www/js/function.js
@@ -212,8 +212,8 @@ function addmenu(){
 						var userin=username+' <cite>'+userdetail.email+'</cite>'
 						jQuery('#uname').html(userin);
 						jQuery('#uname1').html(username);
-						var picture='<img src="'+window.localStorage.getItem("picture")+'"   class="img-responsive">';
-						jQuery('#proicon').html(picture);
+						var picture='<img src="'+window.localStorage.getItem("picture")+'"  style="height: 50px;width: 50px;" class="img-responsive">';
+						jQuery('.user-thumb').html(picture);
 				  
 				  
 				  }else
@@ -250,7 +250,7 @@ function addmenu(){
 						var userin=username+' <cite>'+userdetail.email+'</cite>'
 						jQuery('#uname').html(userin);
 						
-						var picture='<img src="'+window.localStorage.getItem("picture")+'" style="height: 50px;"  class="img-responsive">';
+						var picture='<img src="'+window.localStorage.getItem("picture")+'" style="height: 50px;width: 50px;"  class="img-responsive">';
 						jQuery('.user-thumb').html(picture);
 				  
 				  
