83cb5886ca7bcb96c66082d4ab68eaf0ee110227
Ravikant <rvgupta444@gmail.com>
2017-04-25 23:24:03 +0530

    jhgjhg

--- www/js/left-menuhtml.js
+++ www/js/left-menuhtml.js
@@ -8,67 +8,67 @@
  menuhtml+='<ul class="gw-submenu">';
  menuhtml+='<li><a  onClick="slide(\'compose-sms.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> Compose SMS</a></li>';
  menuhtml+='<li><a onClick="slide(\'all-sent-sms.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> All Sent SMS</a></li>';
- menuhtml+='<li><a href="schedule-sms.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Schedule SMS</a>';
- menuhtml+='<li><a href="quick-compose.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Quick Compose</a></li>';
- menuhtml+='<li><a href="quick-sent-item.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Quick Sent Item</a></li>';
+ menuhtml+='<li><a onClick="slide(\'schedule-sms.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> Schedule SMS</a>';
+ menuhtml+='<li><a onClick="slide(\'quick-compose.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> Quick Compose</a></li>';
+ menuhtml+='<li><a onClick="slide(\'quick-sent-item.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> Quick Sent Item</a></li>';
  menuhtml+='</ul>';
  menuhtml+='</li>';
  menuhtml+='<li class="init-arrow-down"> <a href="javascript:void(0)"> <span class="gw-menu-text"><i class="fa fa-phone"></i> Miss Call Alerts</span> <b class="gw-arrow icon-arrow-up8"></b> </a>';
  menuhtml+='<ul class="gw-submenu">';
- menuhtml+='<li><a href="view-miss-call-alerts.html"><i class="fa fa-angle-right" aria-hidden="true"></i> View Miss Call Alerts</a></li>';
+ menuhtml+='<li><a onClick="slide(\'view-miss-call-alerts.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> View Miss Call Alerts</a></li>';
  menuhtml+='</ul>';
  menuhtml+='</li>';
  menuhtml+='<li class="init-arrow-down"> <a href="javascript:void(0)"> <span class="gw-menu-text"><i class="fa fa-th"></i> Short/Long Code</span> <b></b> </a>';
  menuhtml+='<ul class="gw-submenu">';
- menuhtml+='<li><a href="view-short-long-code.html"><i class="fa fa-angle-right" aria-hidden="true"></i> View Short/Long Code</a></li>';
+ menuhtml+='<li><a onClick="slide(\'view-short-long-code.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> View Short/Long Code</a></li>';
  menuhtml+='</ul>';
  menuhtml+='</li>';
 
  menuhtml+='<li class="init-arrow-down"> <a href="javascript:void(0)"> <span class="gw-menu-text"> <i class="fa fa-file"></i> Others</span> <b></b> </a>';
  menuhtml+='<ul class="gw-submenu">';
- menuhtml+='<li><a href="add-template.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Add Template</a></li>';
- menuhtml+='<li><a href="view-template.html"><i class="fa fa-angle-right" aria-hidden="true"></i> View Template</a></li>';
- menuhtml+='<li><a href="add-group.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Add Group</a></li>';
- menuhtml+='<li><a href="view-group.html"><i class="fa fa-angle-right" aria-hidden="true"></i> View Group</a></li>';
+ menuhtml+='<li><a onClick="slide(\'add-template.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> Add Template</a></li>';
+ menuhtml+='<li><a onClick="slide(\'view-template.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> View Template</a></li>';
+ menuhtml+='<li><a onClick="slide(\'add-group.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> Add Group</a></li>';
+ menuhtml+='<li><a onClick="slide(\'view-group.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> View Group</a></li>';
  menuhtml+='</ul>';
  menuhtml+='</li>';
 
  menuhtml+='<li class="init-arrow-down"> <a href="javascript:void(0)"> <span class="gw-menu-text"> <i class="fa fa-user"></i> Users</span> <b></b> </a>';
  menuhtml+='<ul class="gw-submenu">';
- menuhtml+='<li><a href="add-user.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Add User</a></li>';
- menuhtml+='<li><a href="view-user.html"><i class="fa fa-angle-right" aria-hidden="true"></i> View User</a></li>';
+ menuhtml+='<li><a onClick="slide(\'add-user.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> Add User</a></li>';
+ menuhtml+='<li><a onClick="slide(\'view-user.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> View User</a></li>';
  menuhtml+='</ul>';
  menuhtml+='</li>';
 
  menuhtml+='<li class="init-arrow-down"> <a href="javascript:void(0)"> <span class="gw-menu-text"> <i class="fa fa-users"></i> Reseller</span> <b></b> </a>';
  menuhtml+='<ul class="gw-submenu">';
- menuhtml+='<li><a href="add-reseller.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Add Reseller</a></li>';
- menuhtml+='<li><a href="view-reseller.html"><i class="fa fa-angle-right" aria-hidden="true"></i> View Reseller</a></li>';
+ menuhtml+='<li><a onClick="slide(\'add-reseller.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> Add Reseller</a></li>';
+ menuhtml+='<li><a onClick="slide(\'view-reseller.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> View Reseller</a></li>';
  menuhtml+='</ul>';
  menuhtml+='</li>';
 
  menuhtml+='<li class="init-arrow-down"> <a href="javascript:void(0)"> <span class="gw-menu-text"> <i class="fa fa-cogs"></i> Settings</span> <b></b> </a>';
  menuhtml+='<ul class="gw-submenu">';
 
- menuhtml+='<li><a href="changeusertype.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Change User Type </a></li>';
- menuhtml+='<li><a href="transfer-credits.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Transfer Credits </a></li>';
- menuhtml+='<li><a href="transsactionin.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Transactions In </a></li>';
- menuhtml+='<li><a href="transsactionout.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Transactions Out </a></li>';
- menuhtml+='<li><a href="smsapi.html"><i class="fa fa-angle-right" aria-hidden="true"></i> HTTP API </a></li>';
+ menuhtml+='<li><a onClick="slide(\'changeusertype.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> Change User Type </a></li>';
+ menuhtml+='<li><a onClick="slide(\'transfer-credits.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> Transfer Credits </a></li>';
+ menuhtml+='<li><a onClick="slide(\'transsactionin.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> Transactions In </a></li>';
+ menuhtml+='<li><a onClick="slide(\'transsactionout.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> Transactions Out </a></li>';
+ menuhtml+='<li><a onClick="slide(\'smsapi.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> HTTP API </a></li>';
 
- menuhtml+='<li><a href="bulksmsapi.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Bulk SMS API </a></li>';
- menuhtml+='<li><a href="xmlapi.html"><i class="fa fa-angle-right" aria-hidden="true"></i> XML API </a></li>';
- menuhtml+='<li><a href="credit-api.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Credits API </a></li>';
- menuhtml+='<li><a href="delivery-api.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Delivery API </a></li>';
- menuhtml+='<li><a href="view-sender-ids.html"><i class="fa fa-angle-right" aria-hidden="true"></i> View Sender IDs </a></li>';
- menuhtml+='<li><a href="viewactualcredits.html"><i class="fa fa-angle-right" aria-hidden="true"></i> View Actual Credits </a></li>';
- menuhtml+='<li><a href="send-notifications.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Send Notifications </a></li>';
- menuhtml+='<li><a href="delivery-cutting.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Delivery Cutting </a></li>';
- menuhtml+='<li><a href="delivery-cutting-users-list.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Delivery Cutting Users List</a></li>';
- menuhtml+='<li><a href="website-settings.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Website Settings</a></li>';
- menuhtml+='<li><a href="check-dnd-number.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Check DND Number</a></li>';
- menuhtml+='<li><a href="block-number.html"><i class="fa fa-angle-right" aria-hidden="true"></i> Block Number</a></li>';
- menuhtml+='<li><a href="my-transactions.html"><i class="fa fa-angle-right" aria-hidden="true"></i> My Transactions </a></li>';
+ menuhtml+='<li><a onClick="slide(\'bulksmsapi.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> Bulk SMS API </a></li>';
+ menuhtml+='<li><a onClick="slide(\'xmlapi.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> XML API </a></li>';
+ menuhtml+='<li><a onClick="slide(\'credit-api.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> Credits API </a></li>';
+ menuhtml+='<li><a onClick="slide(\'delivery-api.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> Delivery API </a></li>';
+ menuhtml+='<li><a onClick="slide(\'view-sender-ids.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> View Sender IDs </a></li>';
+ menuhtml+='<li><a onClick="slide(\'viewactualcredits.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> View Actual Credits </a></li>';
+ menuhtml+='<li><a onClick="slide(\'send-notifications.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> Send Notifications </a></li>';
+ menuhtml+='<li><a onClick="slide(\'delivery-cutting.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> Delivery Cutting </a></li>';
+ menuhtml+='<li><a onClick="slide(\'delivery-cutting-users-list.html\')" href=""><i class="fa fa-angle-right" aria-hidden="true"></i> Delivery Cutting Users List</a></li>';
+ menuhtml+='<li><a onClick="slide(\'website-settings.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> Website Settings</a></li>';
+ menuhtml+='<li><a onClick="slide(\'check-dnd-number.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> Check DND Number</a></li>';
+ menuhtml+='<li><a onClick="slide(\'block-number.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> Block Number</a></li>';
+ menuhtml+='<li><a onClick="slide(\'my-transactions.html\')" ><i class="fa fa-angle-right" aria-hidden="true"></i> My Transactions </a></li>';
 
  menuhtml+='</ul>';
  menuhtml+='</li>';
@@ -76,24 +76,24 @@
  document.getElementById("menu").innerHTML =menuhtml;
 
  var footerhtml='<div class="row">';
- footerhtml+='<div class="col-sm-3 col-xs-3 col-sm-3-me"><a href="index.html"><i class="fa fa-home fav-icon"></i><br>Dashboard </a></div>';
+ footerhtml+='<div class="col-sm-3 col-xs-3 col-sm-3-me"><a onClick="slide(\'index.html\')" ><i class="fa fa-home fav-icon"></i><br>Dashboard </a></div>';
 
-  footerhtml+='<div class="col-sm-3 col-xs-3 col-sm-3-me"><a href="quick-compose.html"><i class="fa fa-envelope-o fav-icon"></i><br>Quick SMS </a></div>';
+  footerhtml+='<div class="col-sm-3 col-xs-3 col-sm-3-me"><a onClick="slide(\'quick-compose.html\')" ><i class="fa fa-envelope-o fav-icon"></i><br>Quick SMS </a></div>';
 
  footerhtml+='<div class="col-sm-3 col-xs-3 col-sm-3-me">';
- footerhtml+='<a href="quick-sent-item.html">';
+ footerhtml+='<a onClick="slide(\'quick-sent-item.html\')" >';
  footerhtml+='<i class="fa fa-pencil-square-o fav-icon"></i><br>';
  footerhtml+='Sent Item</a> </div>';
 
  footerhtml+='<div class="col-sm-3 col-xs-3 col-sm-3-me">';
- footerhtml+='<a href="view-group.html">';
+ footerhtml+='<a onClick="slide(\'view-group.html\')" >';
  footerhtml+='<i class="fa fa-users fav-icon"></i><br>';
  footerhtml+='Group </a></div>';
 
 
  footerhtml+='<div class="col-sm-3 col-xs-3 col-sm-3-me">';
  footerhtml+='<span class="notifactions-icon">12</span>';
- footerhtml+='<a href="notification.html">';
+ footerhtml+='<a onClick="slide(\'notification.html\')" >';
  footerhtml+='<i class="fa fa-bell fav-icon"></i><br>';
  footerhtml+='Notification </a></div>';
 
